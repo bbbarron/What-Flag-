c5e48db9a96cf40fe50f09ee9a4a79b6bfc278dd
Barry Barron <barrybbarron@gmail.com>
2022-07-31 18:11:09 -0700

    Final version before loading additional countries. Added score, total questions answered, and added the ability to quit/restart giving final score

--- What Flag?/ContentView.swift
+++ What Flag?/ContentView.swift
@@ -1,10 +1,12 @@
 //
 //  ContentView.swift
 //  What Flag?
+// based on Project 2 of the the Hacking with SwiftUI
+// series (Guess the Flag)
 //
 //  Created by Barry Barron on 7/29/22.
 //
-     
+
 import SwiftUI
 
 struct ContentView: View {
@@ -12,13 +14,16 @@ struct ContentView: View {
     @State private var scoreTitle = ""
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
-
+    @State private var score = 0
+    @State private var totalQuestions = 0
+    @State private var gameOver = false
+    
     var body: some View {
         ZStack {
             
             LinearGradient(gradient:Gradient(colors:[.indigo, .purple]), startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
-           
+            
             VStack (spacing:30){
                 VStack {
                     Text("Tap the flag of")
@@ -38,29 +43,59 @@ struct ContentView: View {
                             .clipShape(Capsule())
                             .shadow(color: .black, radius: 5)
                     }
+                   
+                    }
+                    Button(" Restart ")  {
+                        gameOver = true
+                    }
+                    .background(Color.white)
+                    .foregroundColor(Color.red)
+                VStack (spacing: 10) {  // Add section to show score at bottom of screen
+                                   Text("Your Score is:")
+                                   .foregroundColor(.white)
+                                       .padding(.top, 20)
+                                   Text("\(score)")
+                                       .foregroundColor(.white)
+                                       .fontWeight(.black)
+                                       .font(.largeTitle)
+                                    Text("Total questions played: \(totalQuestions)")
+                                        .foregroundColor(.white)
                 }
+
             }
         }
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text("Your score is \(score)")
         }
-}
+        .alert("Restarting...", isPresented: $gameOver) {
+            Button("Restart", action: reset)
+            Button("Cancel", role: .cancel) {}
+        } message: {
+            Text("Your final score was \(score) correct out of \(totalQuestions) questions")
+        }
+    }
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
-        scoreTitle = "correct"
+            scoreTitle = "Correct!"
+            score += 1
         } else {
-            scoreTitle = "wrong"
+            scoreTitle = "Sorry, that is incorrect. That's the flag of \(countries[number])"
         }
-        
-        showingScore =  true
-    }
+            totalQuestions += 1
+            showingScore = true
+        }
     
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
+func reset () {
+    countries.shuffle()
+    score = 0
+    correctAnswer = Int.random(in: 0...2)
+    totalQuestions = 0
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -69,3 +104,4 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+}
