1359a4b11d0c99cdf293e502ccbf6e3f6be281f1
Barry Barron <barrybbarron@gmail.com>
2022-08-21 16:46:02 -0700

    Final-Final (God willing!) Project complete, all challenges completed and additional animations added

--- What Flag?/ContentView.swift
+++ What Flag?/ContentView.swift
@@ -29,6 +29,7 @@ struct ContentView: View {
     @State private var score = 0
     @State private var totalQuestions = 0
     @State private var gameOver = false
+    @State private var selectedFlag = -1
     
     var body: some View {
         ZStack {
@@ -36,47 +37,72 @@ struct ContentView: View {
             LinearGradient(gradient:Gradient(colors:[.indigo, .purple]), startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             
-            VStack (spacing:30){
-                VStack {
-                    Text("Tap the flag of")
-                        .foregroundColor(.white)
-                        .font(.subheadline.weight(.semibold))
-                    Text(countries[correctAnswer])
-                        .foregroundColor(.white)
-                        .font(.largeTitle.weight(.semibold))
-                }
+            VStack {
+                Spacer()
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.white)
                 
-                ForEach(0..<3) {number in
-                    Button {
-                        flagTapped(number)
-                    } label: {
-                        FlagImage(name: countries[number])
+                VStack (spacing:15){
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.semibold))
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
                     }
-                   
+                    
+                    ForEach(0..<3) {number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            FlagImage(name: countries[number])
+                        }
+                        .rotation3DEffect(.degrees(selectedFlag == number ? 360 : 0), axis: (x: 0, y: 1, z: 0))
+                        .opacity(selectedFlag == -1 || selectedFlag == number ? 1.0 : 0.25)
+                        .scaleEffect(selectedFlag == -1 || selectedFlag == number ? 1.0 : 0.25)
+                        
+                        .animation(.default, value: selectedFlag)
+                        
                     }
                     Button(" Restart ")  {
                         gameOver = true
                     }
                     .background(Color.white)
                     .foregroundColor(Color.red)
-                VStack (spacing: 10) {  // Add section to show score at bottom of screen
-                                   Text("Your Score is:")
-                                   .foregroundColor(.white)
-                                       .padding(.top, 20)
-                                   Text("\(score)")
-                                       .foregroundColor(.white)
-                                       .fontWeight(.black)
-                                       .font(.largeTitle)
-                                    Text("Total questions played: \(totalQuestions)")
-                                        .foregroundColor(.white)
+                    VStack (spacing: 10) {  // Add section to show score at bottom of screen
+                        Text("Your Score is:")
+                            .foregroundColor(.black)
+                            .padding(.top, 20)
+                        Text("\(score)")
+                            .foregroundColor(.black)
+                            .fontWeight(.black)
+                            .font(.largeTitle)
+                        Text("Total questions played: \(totalQuestions)")
+                        .foregroundColor(.black)                }
+                    
                 }
-
+                
+                
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.thinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .padding()
+                
+                Spacer()
+                Spacer()
+                
+                
             }
+            
         }
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
             Text("Your score is \(score)")
+            Spacer()
+            
         }
         .alert("Restarting...", isPresented: $gameOver) {
             Button("Restart", action: reset)
@@ -84,33 +110,36 @@ struct ContentView: View {
         } message: {
             Text("Your final score was \(score) correct out of \(totalQuestions) questions")
         }
+        
     }
     func flagTapped(_ number: Int) {
+        selectedFlag = number
         if number == correctAnswer {
             scoreTitle = "Correct!"
             score += 1
         } else {
             scoreTitle = "Sorry, that is incorrect. That's the flag of \(countries[number])"
         }
-            totalQuestions += 1
-            showingScore = true
-        }
+        totalQuestions += 1
+        showingScore = true
+    }
     
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        selectedFlag = -1
     }
-func reset () {
-    countries.shuffle()
-    score = 0
-    correctAnswer = Int.random(in: 0...2)
-    totalQuestions = 0
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    func reset () {
+        countries.shuffle()
+        score = 0
+        correctAnswer = Int.random(in: 0...2)
+        totalQuestions = 0
     }
-}
-
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+    
 }
